4f378516c418903d82dc324ef65169f378771269
omarkusan <omarkusan@gmail.com>
2017-09-22 01:58:20 -0500

    Hello World Service returns a Bean

--- restful-web-services/src/main/java/com/omar/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/omar/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -15,4 +15,9 @@ public class HelloWorldController {
 	public String helloWorld(){
 		return "Hello World";
 	}
+	//hello-world-bean
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean(){
+		return new HelloWorldBean("Hello World");
+	}
 }
