9280a3105516a0a9d3a152336141b024abf04137
omarkusan <omarkusan@gmail.com>
2017-09-22 01:48:20 -0500

    Creating a Hello World Service

---
+++ restful-web-services/src/main/java/com/omar/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.omar.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+	//GET 
+	//URI - /hello-world
+	//method - "Hello World"
+	@GetMapping(path="/hello-world")
+	public String helloWorld(){
+		return "Hello World";
+	}
+}
